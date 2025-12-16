fdf6bd18de899ea73952695ccf8f25fa83edc471
ankita <ankita@ankitas-MacBook-Air.local>
2025-12-16 22:29:40 +0530

    add message endpoints

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
-// import messageRoutes from './routes/message.route.js';
+import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -14,7 +14,7 @@ app.use(express.json()); // middleware to parse data sent in a request --> req.b
 app.use(cookieParser()); // to get req.cookies
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/message", messageRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(3000, () => {
     console.log("listening on 3000 for ankita");
